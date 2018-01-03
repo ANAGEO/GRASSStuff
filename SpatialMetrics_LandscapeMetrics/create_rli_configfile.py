8a287746cfe56f3e06eceb664b4409561a41ae1d
tais <tgrippa@ulb.ac.be>
2018-01-03 09:49:00 +0100

    make part of the scritp multiprocessed and add security check for raster to exists and have the same spatial resolution and same extent

--- SpatialMetrics_LandscapeMetrics/create_rli_configfile.py
+++ SpatialMetrics_LandscapeMetrics/create_rli_configfile.py
@@ -1,22 +1,51 @@
-######## FUNCTION CREATING THE CONFIGURATION FILE FOR R.LI GRASS GIS'S MODULE.
-#### Create the configuration file for r.li using landscape units defined by vector layer polygons. 
-#### The script will create several configuration corresponding to the different landcover where spatial metrics have to be computed in the same landscape units
-## 'listoflandcoverraster' wait for a list containing the name of landcover raster to be used
-## 'landscape_polygons' wait for a vector layer of polygons that correspond to the landscape units to be used for analysis 
-## 'returnlistpath' wait for a boolean value (True or False) depending if the list of path to the configuration file is desired as return of the function
+##### Function that create the r.li configuration file for a list of landcover raster.
+### It enable to create in one function as many configuration file as the number of raster provided in 'listoflandcoverraster'.
+### It could be use only in case study with a several landcover raster and only one landscape unit layer.
+### So, the landscape unit layer if fixed and there are the landcover raster which change. 
+# 'listoflandcoverraster' wait for a list with the name (string) of landcover rasters.
+# 'landscape_polygons' wait for the name (string) of the vector layer containing the polygons to be used as landscape units.
+# 'returnlistpath' wait for a boolean value (True/False) according to the fact that a list containing the path to the configuration files is desired.
+# 'ncores' wait for a integer corresponding to the number of desired cores to be used for parallelization.
 
-####### TODO: Add check if raster provided in 'listoflandcoverraster' exists
-####### TODO: Add check of same spatial resolution (and extend) between the different landcover raster in the list
+# Import libraries for multiprocessing 
+import multiprocessing
+from multiprocessing import Pool
+from functools import partial 
 
-import sys,os
-import grass.script as gscript
-from grass.script import core as grass
+# Function that copy the landscape unit raster masks on a new layer with name corresponding to the current 'landcover_raster'
+def copy_landscapeunitmasks(current_landcover_raster,base_landcover_raster,landscape_polygons,landscapeunit_bbox,cat):
+    ### Copy the landscape units mask for the current 'cat'
+    # Define the name of the current "current_landscapeunit_rast" layer
+    current_landscapeunit_rast=current_landcover_raster.split("@")[0]+"_"+landscape_polygons.split("@")[0]+"_"+str(cat)          
+    base_landscapeunit_rast=base_landcover_raster.split("@")[0]+"_"+landscape_polygons.split("@")[0]+"_"+str(cat)          
+    # Copy the the landscape unit created for the first landcover map in order to match the name of the current landcover map
+    gscript.run_command('g.copy', overwrite=True, quiet=True, raster=(base_landscapeunit_rast,current_landscapeunit_rast))
+    # Add the line to the maskedoverlayarea variable
+    maskedoverlayarea="MASKEDOVERLAYAREA "+current_landscapeunit_rast+"|"+landscapeunit_bbox[cat]
+    return maskedoverlayarea
 
-def create_rli_configfile(listoflandcoverraster,landscape_polygons,returnlistpath=False):
+# Function that create the r.li configuration file for the base landcover raster and then for all the binary rasters
+def create_rli_configfile(listoflandcoverraster,landscape_polygons,returnlistpath=True,ncores=2):
     # Check if 'listoflandcoverraster' is not empty
     if len(listoflandcoverraster)==0:
         sys.exit("The list of landcover raster is empty and should contain at least one raster name")
-    
+    # Check if rasters provided in 'listoflandcoverraster' exists to avoid error in mutliprocessing 
+    for cur_rast in listoflandcoverraster:
+        try:
+            mpset=cur_rast.split("@")[1]
+        except:
+            mpset=""
+        if cur_rast not in gscript.list_strings(type='raster',mapset=mpset):
+            sys.exit('Raster <%s> not found' %categorical_raster)
+    # Check if rasters provided in 'listoflandcoverraster' have the same extend and spatial resolution 
+    raster={}
+    for x, rast in enumerate(raster_list):
+        raster[x]=gscript.raster_info(rast)
+    key_list=raster.keys()
+    for x in key_list[1:]:
+        for info in ('north','south','east','west','ewres','nsres'):
+            if not raster[0][info]==raster[x][info]:
+                sys.exit("Some raster provided in the list have different spatial resolution or extend, please check")    
     # Get the version of GRASS GIS 
     version=grass.version()['version'].split('.')[0]
     # Define the folder to save the r.li configuration files
@@ -25,24 +54,20 @@ def create_rli_configfile(listoflandcoverraster,landscape_polygons,returnlistpat
     else: 
         rli_dir=os.path.join(os.environ['HOME'],".grass"+version,"r.li")
     if not os.path.exists(rli_dir):
-        os.makedirs(rli_dir) 
-    
+        os.makedirs(rli_dir)
     ## Create an ordered list with the 'cat' value of landscape units to be processed.
     list_cat=[int(x) for x in gscript.parse_command('v.db.select', quiet=True, map=landscape_polygons, column='cat', flags='c')]
     list_cat.sort()
-
     # Declare a empty dictionnary which will contains the north, south, east, west values for each landscape unit
     landscapeunit_bbox={}
     # Declare a empty list which will contain the path of the configation files created
     listpath=[]
     # Declare a empty string variable which will contains the core part of the r.li configuration file
     maskedoverlayarea=""
-    
     # Duplicate 'listoflandcoverraster' in a new variable called 'tmp_list'
     tmp_list=list(listoflandcoverraster)
     # Set the current landcover raster as the first of the list
     base_landcover_raster=tmp_list.pop(0) #The pop function return the first item of the list and delete it from the list at the same time
-    
     # Loop trough the landscape units
     for cat in list_cat:
         # Extract the current landscape unit polygon as temporary vector
@@ -86,21 +111,17 @@ def create_rli_configfile(listoflandcoverraster,landscape_polygons,returnlistpat
         maskedoverlayarea=""
         # Set the current landcover raster as the first of the list
         current_landcover_raster=tmp_list.pop(0) #The pop function return the first item of the list and delete it from the list at the same time
-        # Loop trough the landscape units
-        for cat in list_cat:
-            # Define the name of the current "current_landscapeunit_rast" layer
-            current_landscapeunit_rast=current_landcover_raster.split("@")[0]+"_"+landscape_polygons.split("@")[0]+"_"+str(cat)          
-            base_landscapeunit_rast=base_landcover_raster.split("@")[0]+"_"+landscape_polygons.split("@")[0]+"_"+str(cat)          
-            # Copy the the landscape unit created for the first landcover map in order to match the name of the current landcover map
-            gscript.run_command('g.copy', overwrite=True, quiet=True, raster=(base_landscapeunit_rast,current_landscapeunit_rast))
-            # Add the line to the maskedoverlayarea variable
-            maskedoverlayarea+="MASKEDOVERLAYAREA "+current_landscapeunit_rast+"|"+landscapeunit_bbox[cat]+"\n"
+        # Copy all the landscape units masks for the current landcover raster
+        p=Pool(ncores) #Create a pool of processes and launch them using 'map' function
+        func=partial(copy_landscapeunitmasks,current_landcover_raster,base_landcover_raster,landscape_polygons,landscapeunit_bbox) # Set fixed argument of the function
+        maskedoverlayarea=p.map(func,list_cat) # Launch the processes for as many items in the list and get the ordered results using map function
+        p.close()
+        p.join()
         # Compile the content of the r.li configuration file
         config_file_content="SAMPLINGFRAME 0|0|1|1\n"
-        config_file_content+=maskedoverlayarea
+        config_file_content+="\n".join(maskedoverlayarea)+"\n"
         config_file_content+="RASTERMAP "+current_landcover_raster+"\n"
         config_file_content+="VECTORMAP "+landscape_polygons+"\n"
-
         # Create a new file and save the content
         configfilename=current_landcover_raster.split("@")[0]+"_"+landscape_polygons.split("@")[0]
         path=os.path.join(rli_dir,configfilename)
